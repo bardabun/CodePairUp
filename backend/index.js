781c78ce0fc9785ee93647cac78d5bda9170fc1c
Bar <bardabun@gmail.com>
2023-05-27 18:47:49 +0300

    e

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const codeBlocksRoutes = require("./api/codeblockes");
+const codeBlocksRoutes = require("./api/codeblocks");
 const HttpError = require("./models/http-error");
 
 const app = express();
