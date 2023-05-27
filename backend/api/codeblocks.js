67fb96410d136b2269e6f05d9e21dec9da2b5add
Bar <bardabun@gmail.com>
2023-05-27 19:06:28 +0300

    w

--- backend/api/codeblocks.js
+++ backend/api/codeblocks.js
@@ -5,6 +5,8 @@ const codeBlocksControllers = require("../controllers/codeblocks-controllers");
 const router = express.Router();
 
 // router.get("/:aid", codeBlocksControllers.getCodeBlockById);
-router.get("/", codeBlocksControllers.getCodeBlockById);
+router.get("/", (req, res) => {
+  console.log("Hey");
+});
 
 module.exports = router;
