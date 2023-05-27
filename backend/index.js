ed6a1d14b420fe96de7e419b62d0b4dedfd3fa54
Bar <bardabun@gmail.com>
2023-05-27 18:57:45 +0300

    d

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(bodyParser.json());
+// app.use(cors());
 
 app.use("/api/codeblocks/", codeBlocksRoutes);
 
