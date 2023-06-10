a7e75fa7e04cbbeef5fe5e787393f8d32bd02132
Bar <bardabun@gmail.com>
2023-06-10 14:22:55 +0300

    desc

--- frontend/src/pages/Lobby.js
+++ frontend/src/pages/Lobby.js
@@ -7,6 +7,22 @@ const Lobby = () => {
   return (
     <div className="lobby">
       <h1 className="header--option">Choose Code Block</h1>
+      <p className="intro">
+        This web application assignment involves building a collaborative
+        platform where users can participate in a series of questions. <br />
+        Upon accessing the application, the first user becomes the 'Mentor' and
+        gains access to a fixed code block. <br />
+        Subsequent users can join the session and have the ability to modify
+        their own code block. <br />
+        <br />
+        Notably, all code changes made by any user are instantly propagated{" "}
+        <br />
+        and reflected live for all connected users, thanks to the use of web
+        sockets. <br />
+        {/* In addition to code editing, users can answer the provided questions,
+        submit their responses, and receive timely feedback on their
+        submissions. */}
+      </p>
 
       <div className="buttons--container">
         <Button codeBlockId={1} title={"Sum Number"} />
